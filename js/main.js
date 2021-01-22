e1053d6ca0461478f82b722a335530e13d1b3a6e
pon-pn <75652042+pon-pn@users.noreply.github.com>
2021-01-22 23:11:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 'use strict'
+
+{
+  const year = 2021;
+  const month = 0;
+  
+  // まずは今月分のカレンダー日付を取得していく
+  function getCallenderBody() {
+    // 日付の配列（空の配列を作る）
+    const dates = [];
+    // 末日を作る(末日＝翌月の0番目と同意)
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    // １から末日までループを回してく
+    for(let i = 1; i <= lastDate; i++) {
+      // 空要素に日付を入れ込んでいく
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  getCallenderBody();
+}
