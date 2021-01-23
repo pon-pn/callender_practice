b3db9393fe6303d43b2c26cc46c3c0625328693a
pon-pn <75652042+pon-pn@users.noreply.github.com>
2021-01-23 23:53:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@
   const year = 2021;
   const month = 0;
   
+
+  function getCallenderHead() {
+    const dates = [];
+    // 日付dは先月の末日の日付を表す
+    const d = new Date(year, month, 0).getDate();
+    // 1日までの曜日の個数nは下記のように表す
+    const n = new Date(year, month, 1).getDay();
+
+    // ０からn回までループを回す（ここで先月分の日付を表示）
+    for(let i = 0; i < n; i++) {
+      // 先月分の日付は当月の1日よりi日前（31 ←1日１こ前　30 ←１日より2こ前）
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
   // まずは今月分のカレンダー日付を取得していく
   function getCallenderBody() {
     // 日付の配列（空の配列を作る）
@@ -24,5 +44,6 @@
     console.log(dates);
   }
 
-  getCallenderBody();
+  getCallenderHead();
+  // getCallenderBody();
 }
